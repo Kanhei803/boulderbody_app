0b25763af78191cdb1e379df7cb6dc5663458f4b
Kenjy <kenhiebusiness@gmail.com>
2023-11-20 20:43:29 -0800

    Styling navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/workout">Workout</router-link>
+  <div id="app">
+    <div class="d-flex justify-content-around mx-auto">
+      <div class="d-flex flex-column">
+        <router-link to="/" class="navbar_link"><img class="logo d-flex" src="../assets/images/Boulderbody_Logo.png"
+            alt="Boulderbody Logo"></router-link>
+        <h2><router-link to="/" class="navbar_link">BoulderBody</router-link></h2>
+      </div>
+      <div class="d-flex align-items-end">
+        <h2><router-link to="/about" class="navbar_link mx-2">About</router-link></h2>
+        <h2><router-link to="/workout" class="navbar_link mx-2">Workout</router-link></h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +31,18 @@ export default {
 };
 </script>
 
-<style scoped>
-/* Your component-specific styles here */
+<style>
+#app {
+  max-width: 1200px;
+}
+
+.logo {
+  max-width: 125px;
+  max-height: 125px;
+}
+
+.navbar_link {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
